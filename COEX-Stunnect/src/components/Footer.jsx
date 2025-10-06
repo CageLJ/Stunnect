dd2f006b5a2621860abd7056bcf961d81eaf2af9
Lennart Plug <s1156901@student.hsleiden.nl>
2025-10-06 12:14:31 +0200

    added some css for the footer and a tags, and added the routing options for routing in the footer

--- COEX-Stunnect/src/components/Footer.jsx
+++ COEX-Stunnect/src/components/Footer.jsx
@@ -1,6 +1,16 @@
+import { Link, NavLink } from "react-router-dom"
+
+import "./Footer.css"
 
 export const Footer = () => {
     return (
-        <footer>Footer</footer>
+        <footer>
+            <nav className="navigation">
+                <NavLink to="/" className="link">Posts</NavLink>
+                <NavLink to="/posts/create" className="link">Create Post</NavLink>
+                <NavLink to="/account" className="link">Account</NavLink>
+                {/* <Link to="/">Chat</Link> */}
+            </nav>
+        </footer>
     )
 }
