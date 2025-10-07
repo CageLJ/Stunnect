20dce1570661485c7abb563c0ed75d5712fbd445
lennart <s1156901@student.hsleiden.nl>
2025-10-07 19:33:04 +0200

    added posts page with a postcard that contains als the info, added individual post page (without styling) and changed the route for the individual post page

--- COEX-Stunnect/src/components/posts/PostCard.jsx
+++ COEX-Stunnect/src/components/posts/PostCard.jsx
@@ -4,17 +4,17 @@ import "./PostCard.css"
 export const PostCard = ({post}) => {
     return (
         <div className="card">
-            <div className="infoSection">
-                <Link to={`/user/${post.custom_user_id}`} className="Postuser">
-                    User id:{post.custom_user_id}
-                </Link>
-                <Link className="postTag">Tag id: {post.id}</Link>
-            </div>
-            <div className="textSection">
-                <Link to={`/posts/${post.id}`}>
+            <Link to={`/posts/${post.id}`}>
+                <div className="infoSection">
+                    <Link to={`/user/${post.custom_user_id}`} className="Postuser">
+                        User id:{post.custom_user_id}
+                    </Link>
+                    <Link className="postTag">Tag id: {post.id}</Link>
+                </div>
+                <div className="textSection">
                     <p>{post.text_content}</p>
-                </Link>
-            </div>
+                </div>
+            </Link>
         </div>
     )
 }
