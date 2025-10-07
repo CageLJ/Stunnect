afaed1d4053fdd6be577df552f3b75e84330c78d
lennart <s1156901@student.hsleiden.nl>
2025-10-07 19:25:22 +0200

    added css for the footer so it stays in place when scrolling through the main

--- COEX-Stunnect/src/components/Footer.jsx
+++ COEX-Stunnect/src/components/Footer.jsx
@@ -8,8 +8,8 @@ export const Footer = () => {
             <nav className="navigation">
                 <NavLink to="/" className="link">Posts</NavLink>
                 <NavLink to="/posts/create" className="link">Create Post</NavLink>
-                <NavLink to="/account" className="link">Account</NavLink>
                 <NavLink to="/chat" className="link">Chat</NavLink>
+                <NavLink to="/account" className="link">Account</NavLink>
             </nav>
         </footer>
     )
